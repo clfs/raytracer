32c4fca2a7106ff53527d33c49fc6f023a2a4c7c
Calvin Figuereo-Supraner <mail@calvin.page>
2021-01-05 09:20:05 -0800

    Add missing material file

---
+++ rtlib/src/material.rs
@@ -0,0 +1,81 @@
+use crate::{color::Color, hit::HitRecord, ray::Ray, vec3::Vec3};
+
+pub trait Material {
+    fn scatter(
+        &self,
+        ray_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Color,
+        scattered: &mut Ray,
+    ) -> bool;
+}
+
+pub struct Blank {}
+
+impl Blank {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Material for Blank {
+    fn scatter(
+        &self,
+        _ray_in: &Ray,
+        _rec: &HitRecord,
+        _attenuation: &mut Color,
+        _scattered: &mut Ray,
+    ) -> bool {
+        false
+    }
+}
+
+pub struct Lambertian {
+    pub albedo: Color,
+}
+
+impl Material for Lambertian {
+    fn scatter(
+        &self,
+        _ray_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Color,
+        scattered: &mut Ray,
+    ) -> bool {
+        let mut scatter_direction = rec.normal + Vec3::rand_unit();
+
+        // Catch degenerate scatter direction.
+        if scatter_direction.is_near_zero() {
+            scatter_direction = rec.normal;
+        };
+
+        *scattered = Ray {
+            origin: rec.p,
+            direction: scatter_direction,
+        };
+        *attenuation = self.albedo;
+        true
+    }
+}
+
+pub struct Metal {
+    pub albedo: Color,
+}
+
+impl Material for Metal {
+    fn scatter(
+        &self,
+        ray_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Color,
+        scattered: &mut Ray,
+    ) -> bool {
+        let reflected = ray_in.direction.unit().reflect(rec.normal);
+        *scattered = Ray {
+            origin: rec.p,
+            direction: reflected,
+        };
+        *attenuation = self.albedo;
+        scattered.direction.dot(rec.normal) > 0.0
+    }
+}
